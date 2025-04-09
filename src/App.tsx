5cf11e4799f4fad13574ab9cb4d4368341a87e6c
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-10 00:49:29 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const mqttUser = import.meta.env.VITE_MQTT_USER;
+const mqttPassword = import.meta.env.VITE_MQTT_PASSWORD;
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,7 +29,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more: {mqttUser} {mqttPassword}
       </p>
     </>
   )
