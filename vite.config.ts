538e5afba64393ee9c359465da711385b4cba7e9
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-10 00:35:03 +0300

    sdad

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSingleFile(),
+    basicSsl({
+      name: "test",
+    }),
+  ],
   base: "./",
-})
+});
