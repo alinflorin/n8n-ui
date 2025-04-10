6468c0f13b56cc9314ef4fdb8bdbd648781e7987
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-10 12:25:22 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -19,7 +25,7 @@ function App() {
         Click on the Vite and React logos to learn more.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
