1d9574048fe5e6023837a1cc44d36a0fec38011c
Veronica Dinu <veronica@qoffice.ro>
2025-04-11 17:49:47 +0300

    jhuihuh

--- src/Emails.tsx
+++ src/Emails.tsx
@@ -1,9 +1,161 @@
+
+import "./Emails.css"
+import Accordion from 'react-bootstrap/Accordion';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+
 function Emails() {
 
-    return (
-     <>
-     <p>Emails</p>
-     </>
+  const myInbox = [
+    {
+      subjec: 'vero',
+      to: ['veronica@qoffice'],
+      cc: ['veronica@qoffice'],
+      bcc: ['veronica@qoffice'],
+       body: 'vvvvvvvvvvvero'
+  
+    },
+    {
+      subjec: 'alin',
+      to: ['alin@qoffice'],
+      cc: ['alin@qoffice'],
+      bcc: ['alin@qoffice'],
+       body: 'aaaaaaaaaaaaaaaaaaaalin'
+  
+    },
+  ]
+
+
+  const [formState, setFormState]= useState({
+    subject: '',
+    to: [],
+    cc: [],
+    bcc: [],
+    body: ''
+  })
+
+
+
+  const handleSubjetChange = (e)=>{
+    setFormState(prev=> ({...prev, subject: e.target.value}))
+  };
+
+   
+  
+  
+  return (
+      
+<div className="page-grid">
+
+  <div className="top-row">
+ 
+    <div className="first-column">
+      <h3>Log</h3>
+      <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
+
+   
+    <div className="second-column">
+    <h3>My Inbox</h3>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
+  </div>
+
+
+  <div className="bottom-row">
+  
+    <div className="fixed-column ">
+    <Form.Control type="text" value={formState.subject} onChange={handleSubjetChange} placeholder="Normal text" />
+
+
+    <Form.Select multiple aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
+    <Form.Select aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
+    <Form.Select aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
+    </div>
+
+ 
+    <div className="flex-bottom-column">
+      <div className="flex-item"> 
+         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Body</Form.Label>
+        <Form.Control as="textarea" rows={8} />
+      </Form.Group></div>
+    </div>
+
+
+    <div className="fixed-column">
+    <Button variant="primary">Send</Button>
+    </div>
+  </div>
+</div>
+
+
     );
   }
   
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,24 +6,24 @@ import "./Home.css"
 
 function Home() {
   const cards = [
-    { title: 'Emails', text: 'This....', link: '/emails' },
-    { title: 'Card 2', text: 'This is card 2', link: '/notfound'  },
-    { title: 'Card 3', text: 'This is card 3', link: '/notfound'  },
-    { title: 'Card 4', text: 'This is card 4', link: '/notfound'  },
-    { title: 'Card 5', text: 'This is card 5', link: '/notfound'  },
-    { title: 'Card 6', text: 'This is card 6', link: '/notfound'  },
+    { title: 'Emails', text: 'See more...', link: '/emails' },
+    { title: 'NotFound', text: 'This is card 2', link: '/notfound'  },
+    { title: 'NotFound', text: 'This is card 3', link: '/notfound'  },
+    { title: 'NotFound ', text: 'This is card 4', link: '/notfound'  },
+    { title: 'NotFound', text: 'This is card 5', link: '/notfound'  },
+    { title: 'NotFound', text: 'This is card 6', link: '/notfound'  },
   ];
 
     return (
      <>
    <div className="home-container">
-      <h2>🎴 Responsive Cards with CSS Grid</h2>
+      <h2>🎴 Responsive Cards</h2>
       <div className="grid">
         {cards.map((card, i) => (
           <div key={i} className="card">
             <h4>{card.title}</h4>
             <p>{card.text}</p>
-            <Link to={card.link} ><button> See More</button></Link>
+            <Link to={card.link} ><button> Go to</button></Link>
             
           </div>
         ))}
