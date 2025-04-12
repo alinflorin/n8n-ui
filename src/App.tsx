f8ecec3d1dfd7694f9bbdf0f5249af843090de7f
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-12 13:49:55 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.png";
+import { Outlet } from "react-router";
 import env from "./env";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more. {env.VERSION}
-      </p>
-    </>
+      <div>{env.VERSION}</div>
+    </div>
   );
 }
 
