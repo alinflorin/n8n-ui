c0c122634e667bae4423a3113da89d7ac553dfdc
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-12 15:33:27 +0300

    -

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,8 @@ if (!w.env) {
 export const env = {
     MQTT_USER: (import.meta.env.VITE_MQTT_USER || w.env.MQTT_USER) as string,
     MQTT_PASSWORD: (import.meta.env.VITE_MQTT_PASSWORD || w.env.MQTT_PASSWORD) as string,
-    VERSION: (import.meta.env.VITE_VERSION || w.env.VERSION) as string
+    VERSION: (import.meta.env.VITE_VERSION || w.env.VERSION) as string,
+    API: window.location.href.includes("n8n.internal.huna2.com") ? '/webhook' : `https://${(import.meta.env.VITE_AUTH_USER || w.env.AUTH_USER)}:${(import.meta.env.VITE_AUTH_PASSWORD || w.env.AUTH_PASSWORD)}@n8n.internal.huna2.com/webhook`
 };
 
 export default env;
